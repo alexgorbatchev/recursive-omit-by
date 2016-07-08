970dfddc813f130ac8a8644f56cd0e36b35c6115
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-07-08 11:18:45 -0700

    Always return new object

--- index-es6.js
+++ index-es6.js
@@ -3,16 +3,12 @@ import delObjectPath from 'del-object-path';
 import merge from 'lodash.merge';
 
 export default function recursiveOmitBy(object, callback, ...recursiveIteratorOpts) {
-  let results;
+  const results = merge({}, object);
 
   for (let meta of new RecursiveIterator(object, ...recursiveIteratorOpts)) {
     const shouldOmit = callback(meta);
 
     if (shouldOmit) {
-      if (!results) {
-        results = merge({}, object);
-      }
-
       delObjectPath(results, meta.path.join('.'));
     }
   }
